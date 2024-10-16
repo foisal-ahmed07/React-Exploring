9c402e6ca6f517f87478bb007ee170ac894fa666
Foisal Ahmed <shakhfoisal99@gmail.com>
2024-10-16 22:01:26 +0600

    satate manage learn

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Counter from "./Counter";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  function handleClick() {
+    alert("First clicked");
+  }
+  const handleClickMe = () => {
+    alert("Second clicked");
+  };
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h3> React Core Concepts</h3>
+      <Counter></Counter>
+
+
+
+
+      <button onClick={handleClick}>Click Me 1</button>
+      <button onClick={handleClickMe}>Click Me 2</button>
+      <button
+        onClick={() => {
+          alert("Third Clicked");
+        }}
+      >
+        Click Me 3
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
